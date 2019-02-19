810027d5d60debbd98781bc1e71a2966b484e4d1
Jaewoo Jang <jaewoojang@DN0a249277.SUNet>
2019-02-19 12:17:21 -0800

    added azure integration (incomplete)

--- util/config.js
+++ util/config.js
@@ -4,68 +4,52 @@ config.endpoint = "https://jangj8523.documents.azure.com:443/";
 config.primaryKey = "co5ZQEkpYAnWfgfGSxJ1fS6L73yEr10jLIHDSB0zNiqP7YPQSyt9C90yv1oVPfqMA85LoYidcgFevG6aLn1XfQ";
 
 config.database = {
-   "id": "FamilyDatabase"
+   "id": "CarUser"
 };
 
 config.container = {
-  "id": "FamilyContainer"
+  "id": "Conversation"
 };
 
 config.items = {
    "Andersen": {
        "id": "Anderson.1",
-       "lastName": "Andersen",
-       "parents": [{
-         "firstName": "Thomas"
-     }, {
-             "firstName": "Mary Kay"
-         }],
-     "children": [{
-         "firstName": "Henriette Thaulow",
-         "gender": "female",
-         "grade": 5,
-         "pets": [{
-             "givenName": "Fluffy"
+       "lastName": "Truman",
+       "conversation": ["Stanford University", "Menlo Park", "Boston"],
+     "carInfo": [{
+         "model": "BMW i8",
+         "history": "4.5 years",
+         "recentDestination": [{
+             "restaurants": ["Pompous", "Izakaya", "Nola"],
+             "others" : ["Stanford University", "Menlo Park", "Boston"]
          }]
      }],
-     "address": {
-         "state": "WA",
-         "county": "King",
-         "city": "Seattle"
+     "assistantSetting": {
+         "name": "amicus",
+         "gender": "woman.2",
      }
  },
- "Wakefield": {
-     "id": "Wakefield.7",
-     "parents": [{
-         "familyName": "Wakefield",
-         "firstName": "Robin"
-     }, {
-             "familyName": "Miller",
-             "firstName": "Ben"
-         }],
-     "children": [{
-         "familyName": "Merriam",
-         "firstName": "Jesse",
-         "gender": "female",
-         "grade": 8,
-         "pets": [{
-             "givenName": "Goofy"
-         }, {
-                 "givenName": "Shadow"
-             }]
-     }, {
-             "familyName": "Miller",
-             "firstName": "Lisa",
-             "gender": "female",
-             "grade": 1
-         }],
-     "address": {
-         "state": "NY",
-         "county": "Manhattan",
-         "city": "NY"
-     },
-     "isRegistered": false
-   }
+	"Frank": {
+	       "id": "Frank.1",
+	       "lastName": "Zheng",
+	       "conversation": [{
+	         "firstName": "Thomas"
+	     }, {
+	             "firstName": "Mary Kay"
+	         }],
+	     "carInfo": [{
+	         "model": "BMW i8",
+	         "history": "4.5 years",
+	         "recentDestination": [{
+	             "restaurants": ["Pompous", "Izakaya", "Nola"],
+	             "others" : ["Stanford University", "Menlo Park", "Boston"]
+	         }]
+	     }],
+	     "assistantSetting": {
+	         "name": "amicus",
+	         "gender": "woman.2",
+	     }
+	 }
 };
 
 module.exports = config;
